80fcbc6d71938fb2893fccfb90ee9d74f7268e12
Adrian Carroll <adrian.carroll7@gmail.com>
2019-01-26 21:06:21 +0000

    tested videos controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe "GET new" do
+    it "sets the @user variable" do
+
+      get :new 
+
+      expect(assigns(:user)).to be_truthy 
+    end
+  end
+  
+end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-    context "with authenticated user" do
+    context "with authenticated users" do
       before do
         session[:user_id] = Fabricate(:user).id
         @some_video = Fabricate(:video)
@@ -17,6 +17,52 @@ describe VideosController do
         get :show, id: @some_video.id
 
         response.should render_template :show
+      end
+    end
+
+    context "with unauthenticated users" do
+      before do
+        session.delete(:user_id)
+        @some_video = Fabricate(:video)
+      end
+
+      it "redirects to root page" do
+        get :show, id: @some_video.id
+        response.should redirect_to root_path         
+      end
+    end
+  end
+
+  describe "GET search" do
+    context "with authenticated users" do
+      before do
+        session[:user_id] = Fabricate(:user).id
+        @futurama = Fabricate(:video, title: "Futurama")
+      end
+  
+      it "sets the @video variable" do
+        get :search, search: "rama"
+  
+        assigns(:videos).should == [@futurama]
+      end
+
+      it "renders the search template" do
+        get :search, search: Video.first.title
+
+        response.should render_template :search
+      end
+    end
+
+    context "with unauthenticated users" do
+      before do
+        session[:user_id] = nil
+        @futurama = Fabricate(:video, title: "Futurama")
+      end
+
+      it "redirects to root path" do
+        get :search, search: "rama"
+
+        response.should redirect_to root_path
       end
     end
   end
