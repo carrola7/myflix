56df11bfbbbdf5a009c4611b39a17a1242cde414
Adrian Carroll <adrian.carroll7@gmail.com>
2019-02-04 08:19:36 +0000

    Cleaned up tests for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,5 @@
 class ReviewsController < ApplicationController
   before_filter :ensure_logged_in
-  # def create
-  #   @review = Review.new(review_params)
-
-  #   if @review.save
-  #     flash[:success] = "Your review has been saved"
-  #     redirect_to video_path(@review.video)
-  #   else
-  #     @video = @review.video
-  #     render 'videos/show'
-  #   end
-  # end
 
   def create
     @video = Video.find(params[:video_id])
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,47 +1,5 @@
 require 'spec_helper'
 
-# describe ReviewsController do
-#   describe "POST create" do
-#     context "with valid input" do
-#       before do
-#         user = Fabricate(:user)
-#         @video = Fabricate(:video)
-#         post :create, review: Fabricate.attributes_for(:review, user_id: user.id, video_id: @video.id)
-#       end
-
-#       it "saves a review" do
-#         Review.count.should eq(1)
-#       end
-#       it "redirects to video page" do
-#         response.should redirect_to video_path(@video)
-#       end
-#     end
-
-#     context "with invalid input" do
-#       before do
-#         @user = Fabricate(:user)
-#         @video = Fabricate(:video)
-#       end
-
-#       it "sets the @video variable" do
-#         post :create, review: Fabricate.attributes_for(:review, comment: nil, user_id: @user.id, video_id: @video.id)
-#         (assigns :video).should be_instance_of Video
-#       end
-
-#       it "renders the show video page" do
-#         post :create, review: Fabricate.attributes_for(:review, comment: nil, user_id: @user.id, video_id: @video.id)
-#         response.should render_template "videos/show"
-#       end
-#     end
-
-#     context "with invalid unauthorised user" do
-#       before do
-#         @video = Fabricate(:video)
-#       end
-#     end
-#   end
-# end
-
 describe ReviewsController do
   describe "POST create" do
     let(:video) { Fabricate(:video) }
@@ -66,7 +24,7 @@ describe ReviewsController do
           Review.first.user.should eq(current_user)
         end
       end
-      
+
       context "with invalid input" do
         it "does not create a review" do
           post :create, review: {rating: 4}, video_id: video.id
